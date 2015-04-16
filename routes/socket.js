764aa3ecb6901ce94374569dca90de0da2d80dce
fabiodeuzebio <fabiodeuzebio@gmail.com>
2015-04-15 23:48:12 -0300

    Primeiro Commit

---
+++ routes/socket.js
@@ -0,0 +1,12 @@
+/*
+ * Serve content over a socket
+ */
+
+module.exports = function (socket) {
+  
+  setInterval(function () {
+    socket.emit('send:time', {
+      time: (new Date()).toString()
+    });
+  }, 1000);
+};
