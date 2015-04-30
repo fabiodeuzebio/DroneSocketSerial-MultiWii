d0c273e7858432419dfd3bcf4cbdf8504f6959a7
fabiodeuzebio <fabiodeuzebio@gmail.com>
2015-04-30 12:43:32 -0300

    Exemplo server

--- public/js/filters.js
+++ public/js/filters.js
@@ -8,3 +8,25 @@ angular.module('myApp.filters', []).
       return String(text).replace(/\%VERSION\%/mg, version);
     }
   });
+  
+  
+  var connect = require('connect'),	
+	app = connect().use(connect.static(__dirname + '/views'));
+var socket = require('./routes/socket');	
+var MSP = require('./routes/msp');
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+// Socket.io Communication
+io.sockets.on('connection', socket);
+
+MSP.newFrame.on('new', function(data) {
+	io.sockets.emit('dataToUi', data);
+});
+
+server.listen(3000, function () {
+  console.log('\n=============================================================');
+  console.log('Servidor Node na porta 3000! :)                |');
+  console.log('=============================================================');
+});
